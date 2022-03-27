855271e828300b2a597a1d65779381dc44913696
OSoufian <osoufian@hotmail.fr>
2022-03-27 20:23:47 +0200

    Register aussi fonctionnel

--- src/main/java/com/supinfo/datasource/MyDataSource.java
+++ src/main/java/com/supinfo/datasource/MyDataSource.java
@@ -1,5 +1,7 @@
 package com.supinfo.datasource;
 
+import com.supinfo.myEntities.User;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -44,9 +46,9 @@ public class MyDataSource {
         this.dataSource = dataSource;
     }
 
-    public String userLogin(String username, String password) {
-        if (username != null) checkUserQuery(username);
-        if (username.equals(queryName) && password.equals(queryPassword))
+    public String userLogin(User user) {
+        if (user.getUsername() != null) checkUserQuery(user.getUsername());
+        if (user.getUsername().equals(queryName) && user.getPassword().equals(queryPassword))
             return "success";
         else
             return "invalid";
@@ -71,5 +73,41 @@ public class MyDataSource {
                 sqle.printStackTrace();
             }
         }
+    }
+
+    public String userRegister(User user) {
+        Connection con = null;
+        PreparedStatement ps = null;
+        int i = 0;
+        if (dataSource != null) {
+            try {
+                con = dataSource.getConnection();
+                if (con != null) {
+                    String sql = "INSERT INTO user(username, first_name, name, email, zip_code, password) VALUES(?,?,?,?,?,?)";
+                    ps = con.prepareStatement(sql);
+                    ps.setString(1, user.getUsername());
+                    ps.setString(2, user.getFirstName());
+                    ps.setString(3, user.getName());
+                    ps.setString(4, user.getEmail());
+                    ps.setString(5, user.getZipCode());
+                    ps.setString(6, user.getPassword());
+                    i = ps.executeUpdate();
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            } finally {
+                try {
+                    con.close();
+                    ps.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (i > 0)
+            return "success";
+        else
+            return "failure";
+
     }
 }
--- src/main/java/com/supinfo/myEntities/User.java
+++ src/main/java/com/supinfo/myEntities/User.java
@@ -4,11 +4,16 @@ import com.supinfo.datasource.MyDataSource;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.persistence.*;
 
 @ManagedBean(name = "user")
 @RequestScoped
 public class User {
     private String username;
+    private String firstName;
+    private String name;
+    private String email;
+    private String zipCode;
     private String password;
     private MyDataSource dataSource = new MyDataSource();
 
@@ -30,8 +35,43 @@ public class User {
         this.password = password;
     }
 
-    public String login() {
-
-        return dataSource.userLogin(username, password);
+    public String getFirstName() {
+        return firstName;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String login() {
+        return dataSource.userLogin(this);
+    }
+    public String register() {
+        return dataSource.userRegister(this);
+    }
+
 }
