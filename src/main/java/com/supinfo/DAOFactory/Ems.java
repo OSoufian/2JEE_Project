8b21c9ece81dd74f5fe964ac7248cd8fa56bca8d
OSoufian <osoufian@hotmail.fr>
2022-03-29 20:15:07 +0200

    Fin de journée -> Début d'implémentation session user

--- src/main/java/com/supinfo/DAOFactory/Ems.java
+++ src/main/java/com/supinfo/DAOFactory/Ems.java
@@ -1,12 +1,15 @@
 package com.supinfo.DAOFactory;
 
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
+import javax.persistence.criteria.CriteriaBuilder;
 
 abstract class Ems {
     private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
     @PersistenceContext(type = PersistenceContextType.TRANSACTION)
-    public javax.persistence.EntityManager em = emf.createEntityManager();
+    public EntityManager em = emf.createEntityManager();
+    public CriteriaBuilder cb = em.getCriteriaBuilder();
 }
